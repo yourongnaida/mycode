b3392e9ad35e8600a654ccef7591c1a9010488de
yourongnaida <orangealice476@gmail.com>
2017-10-12 13:11:50 +0800

    Create guessNum.py

---
+++ python/guessNum.py
@@ -0,0 +1,16 @@
+# Guess Number One Time
+import random
+# Generate a random number to be guessed
+number = random.randint(0,99)
+
+print("Guess a magic number between 0 and 100")
+guess = -1
+# Prompt the user to guess the number
+while guess != number:
+      guess = eval(input("Enter your guess:"))
+      if guess == number:
+            print("Yes , the number is ",number)
+      elif guess > number:
+            print("Your guess is too high")
+      else:
+            print("Your guess is too low")
